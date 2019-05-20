9664d394903faeb641e48fb4f046b3507c19f197
Semaias <semaiasrangel@gmail.com>
2019-05-20 09:23:17 -0300

    Adicionei a pasta do SAH

---
+++ Ambiente/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/semaias-rangel/Área de Trabalho/Projeto-Secreto/Ambiente/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
